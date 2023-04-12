1f7549a718597fc62bdc92cb6d170de2cc287397
gamer-mitsuha <107781752+gamer-mitsuha@users.noreply.github.com>
2023-04-13 02:27:58 +0900

    Add caching using Redis {key: text, value: audio_file_path}.

--- tts.py
+++ tts.py
@@ -8,11 +8,20 @@ Example usage:
 """
 
 import os
+import shutil
+import uuid
 
 import azure.cognitiveservices.speech as speechsdk
+import redis
 import typer
 
 app = typer.Typer()
+
+pool = redis.ConnectionPool(host="localhost", port=6379, db=0)
+r = redis.Redis(connection_pool=pool)
+
+DEFAULT_CACHE_DIR = "cache"
+
 
 # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
 speech_config = speechsdk.SpeechConfig(
@@ -22,14 +31,50 @@ speech_config = speechsdk.SpeechConfig(
 # The language of the voice that speaks.
 speech_config.speech_synthesis_voice_name = "ja-JP-NanamiNeural"
 
+
 @app.command()
-def tts(text: str, output: str = "output/sample.wav"):
+def tts(
+    text: str, output: str = "output/sample.wav", cache_dir: str = DEFAULT_CACHE_DIR
+):
     """Simple command line interface for text to speech."""
-    audio_config = speechsdk.audio.AudioOutputConfig(filename=output)
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    # Check if the synthesized speech of the input text is already in the cache.
+    cache_path = r.get(text)
+    if cache_path is not None:
+        print("Found in cache.")
+        cache_path = cache_path.decode("utf-8")
+
+        # Check if the cache is valid. If so, use it as the output.
+        if os.path.exists(cache_path):
+            print("Cache is valid.")
+            # Copy the synthesized speech from the cache to the output path.
+            shutil.copy(cache_path, output)
+            return
+
+        # Otherwise, delete the invalid cache.
+        print("Cache is not valid.")
+        r.delete(text)
+        print("Deleted invalid cache.")
+
+    # If not found in cache or cache is invalid, synthesize the text and store it in the cache.
+    print("Not found in cache. Synthesizing...")
+    cache_path = os.path.join(cache_dir, f"{uuid.uuid4()}.wav")
+
+    # Synthesize the text.
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=cache_path)
     speech_synthesizer = speechsdk.SpeechSynthesizer(
         speech_config=speech_config, audio_config=audio_config
     )
     speech_synthesizer.speak_text_async(text).get()
+
+    # Cache the synthesized speech.
+    r.set(text, cache_path)
+
+    # Copy the synthesized speech from the cache to the output path.
+    shutil.copy(cache_path, output)
 
 
 if __name__ == "__main__":
